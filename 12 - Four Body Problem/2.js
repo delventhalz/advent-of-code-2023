9b5984eb788b25f1c2a46eec2a776b7f2d1b64fe
Zac Delventhal <delventhalz@gmail.com>
2019-12-12 12:23:15 -0600

    Clean up Day 12 part 2
    Copy over vector tuple approach from part 1. Refactor to remove mess
    and make lines of investigation more clear.

--- 12 - Four Body Problem/2.js
+++ 12 - Four Body Problem/2.js
@@ -51,133 +51,99 @@
 
 const lcm = require('compute-lcm');
 
-const positionsToObject = (positions) => Object.fromEntries(
-  positions
-    .map(position => position.replace('<', ''))
-    .map(position => position.replace('>', ''))
-    .map(position => position.trim())
-    .map(position => position.split('='))
-    .map(([key, val]) => [key, Number(val)])
+
+const parsePositionString = (positions) => positions
+  .match(/<x=(-?\d+), y=(-?\d+), z=(-?\d+)>/)
+  .slice(1)
+  .map(Number);
+
+const pluckAt = (matrix, i) => matrix.map(arr => arr[i]);
+const pluckPos = (moons, i) => pluckAt(pluckAt(moons, i), 0);
+
+const countGreater = (nums, target) => nums.filter(num => num > target).length;
+const countLess = (nums, target) => nums.filter(num => num < target).length;
+const velDelta = (positions, pos) => (
+  countGreater(positions, pos) - countLess(positions, pos)
 );
 
-const sort = (nums) => nums.slice().sort((a, b) => a - b);
-const withUniqueItem = (arr, uniqItem) => {
-  const first = arr.indexOf(uniqItem);
-  return arr.filter((item, i) => item !== uniqItem || i === first);
-};
+const accelerate = (moons) => moons.map(moon => (
+  moon.map(([pos, vel], i) => [pos, vel + velDelta(pluckPos(moons, i), pos)])
+));
+const move = (moons) => moons.map(moon => (
+  moon.map(([pos, vel]) => [pos + vel, vel])
+));
 
-const updateVelocity = (moons, key) => {
-  const positions = sort(moons.map(({ pos }) => pos[key]));
 
-  for (const moon of moons) {
-    const pos = moon.pos[key];
-    const withoutMatching = withUniqueItem(positions, pos);
+const toAxisString = (moons, i) => pluckAt(moons, i).join();
 
-    const preceding = withoutMatching.indexOf(pos);
-    const following = withoutMatching.length - preceding - 1;
-    moon.vel[key] += following - preceding;
-  }
+const allArraysHaveLength = (target, ...objs) => objs.every(objOfArrays => (
+  Object.values(objOfArrays).every(({ length }) => length >= target)
+));
+const reverse = (arr) => arr.slice().reverse();
+const diffNums = (nums) => nums.slice(1).map((num, i) => num - nums[i]);
 
-  return moons;
-};
-
-const updateVelocities = (moons) => {
-  updateVelocity(moons, 'x');
-  updateVelocity(moons, 'y');
-  updateVelocity(moons, 'z');
-
-  return moons;
-};
-
-const updatePositions = (moons) => {
-  for (const moon of moons) {
-    const { vel: { x, y, z } } = moon;
-    moon.pos.x += x;
-    moon.pos.y += y;
-    moon.pos.z += z;
-  }
-
-  return moons;
-};
-
-const toKeyString = (moons, key) => [
-  ...moons.map(({ pos }) => pos[key]),
-  ...moons.map(({ vel }) => vel[key])
-].join();
-
-/*
-Notes:
-- Z hits equilibrium at 96237 steps, repeating every step afterwards
-  * -9,-10,-10,0,-1,3,1,-3
-- X hits equilibrium at 135025 steps, repeating every step afterwards
-  * -5,-3,-12,-4,1,-3,3,-1
-- Y hits equilibrium at 231615 steps, repeating every step afterwards
-  * -4,-4,7,-5,1,-1,-3,3
-- X * Y * Z = 3009698170243875 (too high)
-- LCM(X, Y, Z) = 1003232723414625 (too high)
-- These are not clean repeats, i.e. the X of step 96237 !== the X of step 0
-- Off by 1?
-  * YES
-*/
 
 module.exports = (inputs) => {
-  const moons = inputs
-    .map(positionsToObject)
-    .map(pos => ({ pos, vel: { x: 0, y: 0, z: 0 } }));
+  // Moon array of vector tuples: [[xPos, xVel], [yPos, yVel], [zPos, zVel]]
+  let moons = inputs
+    .map(stringPieces => stringPieces.join(','))
+    .map(parsePositionString)
+    .map(([x, y, z]) => [[x, 0], [y, 0], [z, 0]]);
 
-  const xs = {};
-  const ys = {};
-  const zs = {};
+  const xStates = {};
+  const yStates = {};
+  const zStates = {};
   let steps = 0;
 
-  const x = toKeyString(moons, 'x');
-  const y = toKeyString(moons, 'y');
-  const z = toKeyString(moons, 'z');
-  xs[x] = [0];
-  ys[y] = [0];
-  zs[z] = [0];
+  // Record first three states
+  while (steps < 3) {
+    xStates[toAxisString(moons, 0)] = [steps];
+    yStates[toAxisString(moons, 1)] = [steps];
+    zStates[toAxisString(moons, 2)] = [steps];
 
-  while (steps < 231615 * 3 + 1) {
-    updatePositions(updateVelocities(moons));
+    moons = move(accelerate(moons));
     steps += 1;
-
-    const x = toKeyString(moons, 'x');
-    const y = toKeyString(moons, 'y');
-    const z = toKeyString(moons, 'z');
-
-    if (xs[x]) {
-      // console.log(steps, '- X repeats:', xs[x].join());
-      // xs[x].push(steps);
-      // break;
-      xs[x] = null;
-      xs.foo = steps;
-    } else {
-      // xs[x] = [steps];
-    }
-
-    if (ys[y]) {
-      // console.log(steps, '- Y repeats:', ys[y].join());
-      // ys[y].push(steps);
-      // break;
-      ys[y] = null;
-      ys.foo = steps;
-    } else {
-      // ys[y] = [steps];
-    }
-
-    if (zs[z]) {
-      // console.log(steps, '- Z repeats:', zs[z].join());
-      // zs[z].push(steps);
-      // break;
-      zs[z] = null;
-      zs.foo = steps;
-    } else {
-      // zs[z] = [steps];
-    }
   }
 
-  // return lcm(96236, 135024, 231614);
-  return lcm(xs.foo, ys.foo, zs.foo);
+  // Find at least three repeats of first three states
+  while (!allArraysHaveLength(3, xStates, yStates, zStates)) {
+    const x = toAxisString(moons, 0);
+    const y = toAxisString(moons, 1);
+    const z = toAxisString(moons, 2);
+
+    if (xStates[x]) {
+      console.log(steps, '- X repeats:', reverse(xStates[x]).join(', '));
+      xStates[x].push(steps);
+    }
+
+    if (yStates[y]) {
+      console.log(steps, '- Y repeats:', reverse(yStates[y]).join(', '));
+      yStates[y].push(steps);
+    }
+
+    if (zStates[z]) {
+      console.log(steps, '- Z repeats:', reverse(zStates[z]).join(', '));
+      zStates[z].push(steps);
+    }
+
+    moons = move(accelerate(moons));
+    steps += 1;
+  }
+
+  // Confirm the repeats are happening at regular intervals
+  const xIntervals = Object.values(xStates).flatMap(diffNums);
+  const yIntervals = Object.values(yStates).flatMap(diffNums);
+  const zIntervals = Object.values(zStates).flatMap(diffNums);
+
+  console.log();
+  console.log('X intervals:', xIntervals);
+  console.log('Y intervals:', yIntervals);
+  console.log('Z intervals:', zIntervals);
+
+  // Calculate least common multiple of intervals
+  console.log();
+  console.log('LCM:');
+  return lcm(xIntervals[0], yIntervals[0], zIntervals[0]);
 };
 
 
