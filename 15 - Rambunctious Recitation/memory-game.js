64592bf2419482ea84c4a7330d61c2a44c38c5e4
Zac Delventhal <delventhalz@gmail.com>
2020-12-16 14:31:28 -0600

    Refactor Day 15 with swappable data type and explanation
    Cleaned everything up so there is just one implementation with a
    swappable data type. Add an explanatory comment with all of the
    recorded times and memory usage.

--- 15 - Rambunctious Recitation/memory-game.js
+++ 15 - Rambunctious Recitation/memory-game.js
@@ -1,114 +1,111 @@
-// When stopping at 30,000,000 on a 2018 MacBook Air:
-//  - Time:   0.93 sec
-//  - Memory: 116 MB
+// I was fascinated by the vastly different run times when solving
+// this problem with different data types but no changes in logic.
+// Very small changes like initializing an array with a length led to
+// nearly a 300x difference in speed!!
+
+// In order to play around with that, I built a uniform interface for
+// initializing and using five different data types, and ran some tests.
+
+// All tests were run on a 2018 MacBook Air with Node 12.13.0,
+// searching for the 30,000,000th number in Part 2.
+
+// Data Type            | Run Time | Memory Use | Relative Time
+// ---------------------+----------+------------+--------------
+// []                   |  355 sec |  23000 MB* |          380x
+// {}                   |  343 sec |  23000 MB* |          370x
+// new Map()            | 6.86 sec |     234 MB |          7.4x
+// new Array(length)    | 1.22 sec |     238 MB |          1.3x
+// new Uint32Array(...) | 0.93 sec |     116 MB |            1x
+
+// * Memory usage for vanilla JS arrays and objects grew unbounded until
+//   execution stopped or the process hit the max-old-space-size for Node.
+//   Hitting that size limit would cause memory usage to drop to ~300MB
+//   before it started to climb again.
+
+const _initArray = () => {
+  const array = [];
+
+  const get = index => array[index];
+  const set = (index, value) => {
+    array[index] = value;
+  };
+
+  return [get, set];
+};
+
+const _initObject = () => {
+  const object = [];
+
+  const get = key => object[key];
+  const set = (key, value) => {
+    object[key] = value;
+  };
+
+  return [get, set];
+};
+
+const _initMap = () => {
+  const map = new Map();
+
+  const get = key => map.get(key);
+  const set = (key, value) => map.set(key, value);
+
+  return [get, set];
+};
+
+const _initSizedArray = (length) => {
+  const sizedArray = new Array(length);
+
+  const get = index => sizedArray[index];
+  const set = (index, value) => {
+    sizedArray[index] = value;
+  };
+
+  return [get, set];
+};
+
+const _initTypedArray = (length) => {
+  const typedArray = new Uint32Array(new ArrayBuffer(length * 4));
+
+  const get = index => typedArray[index];
+  const set = (index, value) => {
+    typedArray[index] = value;
+  };
+
+  return [get, set];
+};
+
+
+/*** CHANGE THIS VALUE TO CHANGE DATA TYPE ***/
+const initMemory = _initTypedArray;
+
+
 const memoryGame = (startingNumbers, stopAt) => {
-  const memory = new Uint32Array(new ArrayBuffer(stopAt * 4));
+  const [getMemory, setMemory] = initMemory(stopAt);
   const startingLength = startingNumbers.length;
   let last;
 
   for (let i = 0; i < startingLength; i += 1) {
-    last = startingNumbers[i];
-    memory[last] = i;
+    setMemory(startingNumbers[i], i);
   }
 
-  // TypedArrays are populated with 0's, so we have to manually skip past
-  // the iterations where 0 is a valid remembered index
-  memory[0] = startingLength
+  // TypedArrays are populated with 0's, so we have to manually skip
+  // the iterations where 0 is a valid remembered value
+  setMemory(0, startingLength);
   last = startingLength;
 
   for (let i = startingLength + 2; i < stopAt; i += 1) {
-    const remembered = memory[last];
-    const next = remembered !== 0
-      ? i - 1 - remembered
-      : 0;
+    const remembered = getMemory(last);
+    const next = remembered ? i - 1 - remembered : 0;
 
-    memory[last] = i - 1;
+    setMemory(last, i - 1);
     last = next;
   }
 
   return last;
 };
 
-// When stopping at 30,000,000 on a 2018 MacBook Air:
-//  - Time:   1.22 sec
-//  - Memory: 238 MB
-//
-// Note that this solution is slower than a vanilla JS object if
-// `memory` is instantiated without a length.
-const memoryGameWithSizedArray = (startingNumbers, stopAt) => {
-  const memory = new Array(stopAt);
-  let last;
-
-  for (let i = 0; i < startingNumbers.length; i += 1) {
-    last = startingNumbers[i];
-    memory[last] = i;
-  }
-
-  for (let i = startingNumbers.length; i < stopAt; i += 1) {
-    const remembered = memory[last];
-    const next = remembered !== undefined
-      ? i - 1 - remembered
-      : 0;
-
-    memory[last] = i - 1;
-    last = next;
-  }
-
-  return last;
-};
-
-// When stopping at 30,000,000 on a 2018 MacBook Air:
-//  - Time:   6.86 sec
-//  - Memory: 234 MB
-const memoryGameWithMap = (startingNumbers, stopAt) => {
-  const memory = new Map();
-  let last;
-
-  for (let i = 0; i < startingNumbers.length; i += 1) {
-    last = startingNumbers[i];
-    memory.set(last, i);
-  }
-
-  for (let i = startingNumbers.length; i < stopAt; i += 1) {
-    const next = memory.has(last)
-      ? i - 1 - memory.get(last)
-      : 0;
-
-    memory.set(last, i - 1);
-    last = next;
-  }
-
-  return last;
-};
-
-// When stopping at 30,000,000 on a 2018 MacBook Air:
-//  - Time:   343 sec
-//  - Memory: 4 GB
-const memoryGameWithObject = (startingNumbers, stopAt) => {
-  const memory = {};
-  let last;
-
-  for (let i = 0; i < startingNumbers.length; i += 1) {
-    last = startingNumbers[i];
-    memory[last] = i;
-  }
-
-  for (let i = startingNumbers.length; i < stopAt; i += 1) {
-    const remembered = memory[last];
-    const next = remembered !== undefined
-      ? i - 1 - remembered
-      : 0;
-
-    memory[last] = i - 1;
-    last = next;
-  }
-
-  return last;
-};
 
 module.exports = {
-  memoryGame,
-  memoryGameWithSizedArray,
-  memoryGameWithMap,
-  memoryGameWithObject
+  memoryGame
 };
