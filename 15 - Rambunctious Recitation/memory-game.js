2aab6d8aa92c69a746a0cabb527303658d3b301b
Zac Delventhal <delventhalz@gmail.com>
2020-12-16 10:24:29 -0600

    Replace memory Map with a sized Array
    Further speed improvements! Based on some C++ solutions which pointed
    out sized arrays were much faster than hashmaps, I tried instantiating
    the memory object as an Array with a pre-determined size. This was
    over 5x faster than a Map!
    
    It looks like the big speed hit here is for resizes. Maps presumably
    handle these better than vanilla object, but not resizing at all is
    even better.

--- 15 - Rambunctious Recitation/memory-game.js
+++ 15 - Rambunctious Recitation/memory-game.js
@@ -1,4 +1,31 @@
+// Time when stopping at 30,000,000 on a 2018 MacBook Air: 1.272 sec
+//
+// Note that this solution is just as slow as a vanilla JS object if
+// `memory` is instantiated without a length.
 const memoryGame = (startingNumbers, stopAt) => {
+  const memory = new Array(stopAt);
+  let last;
+
+  for (let i = 0; i < startingNumbers.length; i += 1) {
+    last = startingNumbers[i];
+    memory[last] = i;
+  }
+
+  for (let i = startingNumbers.length; i < stopAt; i += 1) {
+    const remembered = memory[last];
+    const next = remembered !== undefined
+      ? i - 1 - remembered
+      : 0;
+
+    memory[last] = i - 1;
+    last = next;
+  }
+
+  return last;
+};
+
+// Time when stopping at 30,000,000 on a 2018 MacBook Air: 6.98 sec
+const memoryGameWithMap = (startingNumbers, stopAt) => {
   const memory = new Map();
   let last;
 
@@ -19,10 +46,7 @@ const memoryGame = (startingNumbers, stopAt) => {
   return last;
 };
 
-// Over 50x slower than the nearly identical Map implementation above.
-// Timed on a 2018 MacBook Air:
-//   - object: 366.9  sec
-//   - Map:      6.98 sec
+// Time when stopping at 30,000,000 on a 2018 MacBook Air: 366.9 sec
 const memoryGameWithObject = (startingNumbers, stopAt) => {
   const memory = {};
   let last;
@@ -47,5 +71,6 @@ const memoryGameWithObject = (startingNumbers, stopAt) => {
 
 module.exports = {
   memoryGame,
+  memoryGameWithMap,
   memoryGameWithObject
 };
