f03c72f96cbcfb5b9a7798ff5d096ce48b645aeb
Zac Delventhal <delventhalz@gmail.com>
2020-12-16 10:17:14 -0600

    Replace memory object with memory Map
    Based on a tip from a fellow advent-of-coder I tried swapping out the
    vanilla JS object for a Map, and discovered it was over 50x faster!
    
    As near as I can tell, the more strict specification for Map allows it
    to be implemented in a more efficient way at the low level. That really
    becomes apparent when you have a ton of members, like the ~30 million
    in part 2 of Day 15.

--- 15 - Rambunctious Recitation/memory-game.js
+++ 15 - Rambunctious Recitation/memory-game.js
@@ -1,4 +1,29 @@
 const memoryGame = (startingNumbers, stopAt) => {
+  const memory = new Map();
+  let last;
+
+  for (let i = 0; i < startingNumbers.length; i += 1) {
+    last = startingNumbers[i];
+    memory.set(last, i);
+  }
+
+  for (let i = startingNumbers.length; i < stopAt; i += 1) {
+    const next = memory.has(last)
+      ? i - 1 - memory.get(last)
+      : 0;
+
+    memory.set(last, i - 1);
+    last = next;
+  }
+
+  return last;
+};
+
+// Over 50x slower than the nearly identical Map implementation above.
+// Timed on a 2018 MacBook Air:
+//   - object: 366.9  sec
+//   - Map:      6.98 sec
+const memoryGameWithObject = (startingNumbers, stopAt) => {
   const memory = {};
   let last;
 
@@ -21,5 +46,6 @@ const memoryGame = (startingNumbers, stopAt) => {
 };
 
 module.exports = {
-  memoryGame
+  memoryGame,
+  memoryGameWithObject
 };
