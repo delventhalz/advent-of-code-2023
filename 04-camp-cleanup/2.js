b98adbe6c4eb8c29074c636c727a48972fe5207e
Zac Delventhal <delventhalz@gmail.com>
2022-12-03 23:15:57 -0600

    Add night-of solution to Day 4

---
+++ 04-camp-cleanup/2.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// --- Part Two ---
+
+// const {  } = require('lodash');
+const { between, count } = require('../lib');
+
+
+module.exports = (inputs) => {
+  const pairs = inputs.map(pair => pair.map(elf => elf.split('-').map(Number)));
+
+  return count(pairs, ([a, b]) => {
+    if (between(a[0], b[0], b[1] + 1) || between(a[1], b[0], b[1] + 1)) {
+      return true;
+    }
+
+    if (between(b[0], a[0], a[1] + 1) || between(b[1], a[0], a[1] + 1)) {
+      return true;
+    }
+
+    return false
+  });
+};
