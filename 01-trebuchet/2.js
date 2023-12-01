25500e5a92eef9769e44e0c35345d656bf5fbdea
Zac Delventhal <delventhalz@gmail.com>
2023-11-30 23:32:00 -0600

    Add Day 01 puzzle text

--- 01-trebuchet/2.js
+++ 01-trebuchet/2.js
@@ -1,9 +1,30 @@
 'use strict';
 
-// --- Day 1 ---
+// --- Part Two ---
+
+// Your calculation isn't quite right. It looks like some of the digits are
+// actually spelled out with letters: one, two, three, four, five, six, seven,
+// eight, and nine also count as valid "digits".
+
+// Equipped with this new information, you now need to find the real first and
+// last digit on each line. For example:
+
+//     two1nine
+//     eightwothree
+//     abcone2threexyz
+//     xtwone3four
+//     4nineeightseven2
+//     zoneight234
+//     7pqrstsixteen
+
+// In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76.
+// Adding these together produces 281.
+
+// What is the sum of all of the calibration values?
 
 const { last } = require('lodash');
 const { parseIfNumber, sum } = require('../lib');
+
 
 const lineToNumbers = (line) => {
   const numbers = [];
@@ -60,3 +81,5 @@ module.exports = (_, rawInputs) => {
 
   return sum(numbers);
 };
+
+// Your puzzle answer was 52834.
