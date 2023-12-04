8a5c12acb0e38532393a3298cf3c515508c0a0d9
Zac Delventhal <delventhalz@gmail.com>
2023-12-03 23:46:17 -0600

    Clean up Day 4, 2023
    Light clean up. Just some variable names and the use of my count util.

--- 04-scratchcards/1.js
+++ 04-scratchcards/1.js
@@ -7,18 +7,18 @@
  * https://adventofcode.com/2023/day/4
  */
 
-import { sum } from '../lib/index.js';
+import { sum, count } from '../lib/index.js';
 
 
 export default function main({ lines }) {
   const points = lines
     .map(line => line.split(': ')[1])
-    .map(numbers => numbers.replaceAll('  ', ' '))
-    .map(numbers => numbers.split(' | '))
-    .map(numbers => numbers.map(nums => nums.split(' ').map(Number)))
+    .map(line => line.replaceAll('  ', ' '))
+    .map(line => line.split(' | '))
+    .map(sides => sides.map(side => side.split(' ').map(Number)))
     .map(([winners, mine]) => {
-      const myWinners = mine.filter(num => winners.includes(num));
-      return myWinners.length === 0 ? 0 : 2 ** (myWinners.length - 1);
+      const wonCount = count(mine, num => winners.includes(num));
+      return wonCount === 0 ? 0 : 2 ** (wonCount - 1);
     });
 
   return sum(points);
--- 04-scratchcards/2.js
+++ 04-scratchcards/2.js
@@ -7,22 +7,22 @@
  * https://adventofcode.com/2023/day/4#part2
  */
 
-import { sum } from '../lib/index.js';
+import { sum, count } from '../lib/index.js';
 
 
 export default function main({ lines }) {
   const cards = lines
     .map(line => line.split(': ')[1])
-    .map(numbers => numbers.replaceAll('  ', ' '))
-    .map(numbers => numbers.split(' | '))
-    .map(numbers => numbers.map(nums => nums.split(' ').map(Number)));
+    .map(line => line.replaceAll('  ', ' '))
+    .map(line => line.split(' | '))
+    .map(sides => sides.map(side => side.split(' ').map(Number)));
 
   const cardCounts = cards.map(() => 1);
 
   cards.forEach(([winners, mine], index) => {
-    const myWinners = mine.filter(num => winners.includes(num)).length;
+    const wonCount = count(mine, num => winners.includes(num));
 
-    for (let i = index + 1; i <= index + myWinners.length; i += 1) {
+    for (let i = index + 1; i <= index + wonCount; i += 1) {
       cardCounts[i] += cardCounts[index];
     }
   });
