94fbb9cf1c2789df807dc2955a260ca06c05724b
Zac Delventhal <delventhalz@gmail.com>
2023-12-07 00:05:54 -0600

    Add initial solution for Day 7, Camel Cards
    I really like my approach here, converting the scores for hands and high
    cards in each place to their own order of magnitude, allowing them to
    be summed into a unique sortable score for each hand... unfortunately I
    screwed it up. On my first pass I only separated each place by a factor
    of 10, which is not enough when you have 13 different cards. It took
    20 minutes of debugging to find my mistake. Ah well.

---
+++ 07-camel-cards/1.js
@@ -0,0 +1,60 @@
+/**
+ * --- Advent of Code 2023 ---
+ *
+ * Day 7: Camel Cards
+ * (Part 1)
+ *
+ * https://adventofcode.com/2023/day/7
+ */
+
+import { countGroups, greatest, count, sum } from '../lib/index.js';
+
+
+const CARDS = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'].reverse();
+
+const toCardValue = card => CARDS.indexOf(card) + 1;
+
+const toCardsValue = hand => hand.slice().reverse()
+  .reduce((total, card, index) => total + toCardValue(card) * 100 ** index, 0);
+
+const toHandValue = (hand) => {
+  const cardCounts = countGroups(hand);
+  const counts =Object.values(cardCounts);
+  const highCount = greatest(counts);
+
+  if (highCount === 5) {
+    return 70000000000;
+  }
+  if (highCount === 4) {
+    return 60000000000;
+  }
+  if (counts.includes(3) && counts.includes(2)) {
+    return 50000000000;
+  }
+  if (highCount === 3) {
+    return 40000000000;
+  }
+  if (count(counts, 2) === 2) {
+    return 30000000000;
+  }
+  if (highCount === 2) {
+    return 20000000000;
+  }
+  if (highCount === 1) {
+    return 10000000000;
+  }
+  throw new Error(`What did I do? (${hand.join('')} -> ${counts}!`);
+};
+
+
+export default function main({ lines }) {
+  const hands = lines
+    .map(line => line.split(' '))
+    .map(([hand, bid]) => [hand.split(''), Number(bid)]);
+
+  hands.sort(([handA], [handB]) => {
+    return (toCardsValue(handA) + toHandValue(handA)) - (toCardsValue(handB) + toHandValue(handB))
+  });
+
+  return sum(hands.map(([_, bid], index) => bid * (index + 1)));
+}
