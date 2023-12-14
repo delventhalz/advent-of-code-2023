8c5470b69e3c479f42f77a69213c71013f86186f
Zac Delventhal <delventhalz@gmail.com>
2023-12-14 09:48:24 -0600

    Use memoization to brute force Day 14, Part 2
    I solved this the night of using logs and math, but I was curious if
    memoization would be fast enough to solve it. Once again, cacheing
    impresses. This can brute forcing through four billion matrix
    manipulations in just under two minutes. Faster than it took me to work
    out the math honestly.

--- 14-parabolic-reflector-dish/2.js
+++ 14-parabolic-reflector-dish/2.js
@@ -7,29 +7,58 @@
  * https://adventofcode.com/2023/day/14#part2
  */
 
+import { memoize } from 'lodash-es';
 import {
   eachMatrix,
   eachUp,
   eachDown,
   eachLeft,
   eachRight,
-  matrixToString
+  matrixToString,
+  stringToMatrix
 } from '../lib/index.js';
 
 
-export default function main({ matrix }) {
-  for (let i = 0; i < 1000000000; i += 1) {
-    // This will never finish: look at the logs, find the cycle, and math it out
-    console.log(`${i + 1}:`);
+const rollUp = memoize((map) => {
+  const matrix = stringToMatrix(map);
 
-    eachMatrix(matrix, (tile, coords) => {
+  eachMatrix(matrix, (tile, coords) => {
+    if (tile === 'O') {
+      let lastOpenSpot = coords;
+      let stopped = false;
+
+      eachUp(matrix, coords, (upTile, upCoords) => {
+        if (!stopped && upTile === '.') {
+          lastOpenSpot = upCoords;
+        } else {
+          stopped = true;
+        }
+      });
+
+      matrix[coords[1]][coords[0]] = '.';
+      matrix[lastOpenSpot[1]][lastOpenSpot[0]] = 'O';
+    }
+  });
+
+  return matrixToString(matrix);
+});
+
+
+const rollLeft = memoize((map) => {
+  const matrix = stringToMatrix(map);
+
+  for (let x = 0; x < matrix[0].length; x += 1) {
+    for (let y = 0; y < matrix.length; y += 1) {
+      const tile = matrix[y][x];
+      const coords = [x, y];
+
       if (tile === 'O') {
         let lastOpenSpot = coords;
         let stopped = false;
 
-        eachUp(matrix, coords, (upTile, upCoords) => {
-          if (!stopped && upTile === '.') {
-            lastOpenSpot = upCoords;
+        eachLeft(matrix, coords, (leftTile, leftCoords) => {
+          if (!stopped && leftTile === '.') {
+            lastOpenSpot = leftCoords;
           } else {
             stopped = true;
           }
@@ -38,89 +67,83 @@ export default function main({ matrix }) {
         matrix[coords[1]][coords[0]] = '.';
         matrix[lastOpenSpot[1]][lastOpenSpot[0]] = 'O';
       }
-    });
-
-    for (let x = 0; x < matrix[0].length; x += 1) {
-      for (let y = 0; y < matrix.length; y += 1) {
-        const tile = matrix[y][x];
-        const coords = [x, y];
-
-        if (tile === 'O') {
-          let lastOpenSpot = coords;
-          let stopped = false;
-
-          eachLeft(matrix, coords, (leftTile, leftCoords) => {
-            if (!stopped && leftTile === '.') {
-              lastOpenSpot = leftCoords;
-            } else {
-              stopped = true;
-            }
-          });
-
-          matrix[coords[1]][coords[0]] = '.';
-          matrix[lastOpenSpot[1]][lastOpenSpot[0]] = 'O';
-        }
-      }
     }
-
-    for (let y = matrix.length - 1; y >= 0; y -= 1) {
-      for (let x = 0; x < matrix[0].length; x += 1) {
-        const tile = matrix[y][x];
-        const coords = [x, y];
-
-        if (tile === 'O') {
-          let lastOpenSpot = coords;
-          let stopped = false;
-
-          eachDown(matrix, coords, (downTile, downCoords) => {
-            if (!stopped && downTile === '.') {
-              lastOpenSpot = downCoords;
-            } else {
-              stopped = true;
-            }
-          });
-
-          matrix[coords[1]][coords[0]] = '.';
-          matrix[lastOpenSpot[1]][lastOpenSpot[0]] = 'O';
-        }
-      }
-    }
-
-    for (let x = matrix[0].length; x >= 0; x -= 1) {
-      for (let y = 0; y < matrix.length; y += 1) {
-        const tile = matrix[y][x];
-        const coords = [x, y];
-
-        if (tile === 'O') {
-          let lastOpenSpot = coords;
-          let stopped = false;
-
-          eachRight(matrix, coords, (rightTile, rightCoords) => {
-            if (!stopped && rightTile === '.') {
-              lastOpenSpot = rightCoords;
-            } else {
-              stopped = true;
-            }
-          });
-
-          matrix[coords[1]][coords[0]] = '.';
-          matrix[lastOpenSpot[1]][lastOpenSpot[0]] = 'O';
-        }
-      }
-    }
-
-    let load = 0;
-
-    eachMatrix(matrix, (tile, [_, y]) => {
-      if (tile === 'O') {
-        load += matrix.length - y;
-      }
-    });
-
-    console.log(load);
-    console.log();
   }
 
+  return matrixToString(matrix);
+});
+
+
+const rollDown = memoize((map) => {
+  const matrix = stringToMatrix(map);
+
+  for (let y = matrix.length - 1; y >= 0; y -= 1) {
+    for (let x = 0; x < matrix[0].length; x += 1) {
+      const tile = matrix[y][x];
+      const coords = [x, y];
+
+      if (tile === 'O') {
+        let lastOpenSpot = coords;
+        let stopped = false;
+
+        eachDown(matrix, coords, (downTile, downCoords) => {
+          if (!stopped && downTile === '.') {
+            lastOpenSpot = downCoords;
+          } else {
+            stopped = true;
+          }
+        });
+
+        matrix[coords[1]][coords[0]] = '.';
+        matrix[lastOpenSpot[1]][lastOpenSpot[0]] = 'O';
+      }
+    }
+  }
+
+  return matrixToString(matrix);
+});
+
+
+const rollRight = memoize((map) => {
+  const matrix = stringToMatrix(map);
+
+  for (let x = matrix[0].length; x >= 0; x -= 1) {
+    for (let y = 0; y < matrix.length; y += 1) {
+      const tile = matrix[y][x];
+      const coords = [x, y];
+
+      if (tile === 'O') {
+        let lastOpenSpot = coords;
+        let stopped = false;
+
+        eachRight(matrix, coords, (rightTile, rightCoords) => {
+          if (!stopped && rightTile === '.') {
+            lastOpenSpot = rightCoords;
+          } else {
+            stopped = true;
+          }
+        });
+
+        matrix[coords[1]][coords[0]] = '.';
+        matrix[lastOpenSpot[1]][lastOpenSpot[0]] = 'O';
+      }
+    }
+  }
+
+  return matrixToString(matrix);
+});
+
+export default function main({ input }) {
+  let map = input;
+
+  for (let i = 0; i < 1000000000; i += 1) {
+    map = rollUp(map);
+    map = rollLeft(map);
+    map = rollDown(map);
+    map = rollRight(map);
+  }
+
+  const matrix = stringToMatrix(map);
   let load = 0;
 
   eachMatrix(matrix, (tile, [_, y]) => {
